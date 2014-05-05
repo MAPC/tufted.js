e867ac1a445ac7b5d077e0217d2adc9ef1c4743a
allthesignals <wilburnforce@gmail.com>
2014-05-05 15:40:57 -0400

    Initial commit, browserified modules split.

---
+++ build.js
@@ -0,0 +1,13 @@
+var basechart = require('./lib/baseChart');
+var barchart = require('./lib/barChart');
+var groupedbarchart = require('./lib/groupedBarChart');
+var linechart = require('./lib/lineChart');
+var stackedbarchart = require('./lib/stackedBarChart');
+
+module.exports = {
+  basechart: basechart,
+  barchart: barchart,
+  groupedbarchart: groupedbarchart,
+  linechart: linechart,
+  stackedbarchart: stackedbarchart
+}
